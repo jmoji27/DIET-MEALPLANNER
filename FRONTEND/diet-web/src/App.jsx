6f63280c4b451fb596fda93fbbebb291b8d6a517
jmoji27 <jaimejesus@ogr.eskisehir.edu.tr>
2025-05-04 16:41:17 +0300

    NavBar

---
+++ FRONTEND/diet-web/src/App.jsx
@@ -0,0 +1,13 @@
+import { div } from "motion/react-client";
+import React from "react";
+import NavMain from "./components/navbar/NavMain";
+
+const App = () => {
+  return (
+    <div>
+      <NavMain />
+    </div>
+  );
+};
+
+export default App;
---
+++ FRONTEND/diet-web/src/components/navbar/NavMain.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const listItems = [
+  { name: "About Us", id: "about" },
+  { name: "Diets", id: "diets" },
+  { name: "Recipes", id: "recipes" },
+  { name: "Meal Planning", id: "mealplanning" },
+];
+
+const NavMain = () => {
+  return (
+    <div
+      className="fixed bg-black flex 
+      justify-between items-center 
+      gap-16 py-3 px-10 left-1/2
+       translate-x-[-50%] top-[20px] 
+       rounded-full backdrop-blur-md
+       bg-opacity-60 text-white 
+       shadow-lg z-10"
+    >
+      <ul className="flex gap-8 text-xl">
+        {listItems.map((item) => (
+          <li key={item.id} className="relative group cursor-pointer">
+            <a href={`#${item.id}`}>{item.name}</a>
+            <span className="absolute left-0 bottom-[-5px] w-0 h-1 rounded-xl bg-gradient-to-r from-blue-500 to-green-500 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+        ))}
+      </ul>
+      <button
+        className="bg-gradient-to-r 
+        from-blue-500 to-green-400 py-1 px-6
+        rounded-3xl shadow-2xl text-white text-lg
+        font-semibold hover:from-blue-600
+        hover:to-green-500 hover:shadow-green-500
+        shadow-blue-500"
+      >
+        Log In
+      </button>
+    </div>
+  );
+};
+
+export default NavMain;
